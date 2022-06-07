e0ff9bae6145131c5c8ada94948775ef71031d70
lielshuker <Lielshuker4@gmail.com>
2022-06-07 15:18:43 +0300

    add forgot password

--- app/auth/auth_controller.py
+++ app/auth/auth_controller.py
@@ -1,15 +1,18 @@
 import json
 from datetime import datetime, timezone, timedelta
-from flask import request, jsonify
-
+from flask import request, jsonify, render_template
 from flask_jwt_extended import create_access_token, unset_jwt_cookies, get_jwt, get_jwt_identity, jwt_required
+from itsdangerous import URLSafeTimedSerializer
 from passlib.handlers.pbkdf2 import pbkdf2_sha256
-
+from flask import current_app
+from app import db
+from app.users.EmailModel import create_message, service, send_message
 from app.users.UserModel import User
+from threading import Thread
 
 
 @jwt_required()
-def profile(user_id, request):
+def profile():
     response_body = {
         "name": "Nagato",
         "about": "Hello! I'm a full stack developer that loves python and javascript"
@@ -34,6 +37,60 @@ def login():
     access_token = create_access_token(identity='username')
     response = {"access_token": access_token}
     return response
+
+
+def forgot_password():
+    email = request.args.get("email", None)
+    user = User.query.filter(User.email == email).first()
+    if not user:
+        return {"msg": "Invalid email address!"}, 401  # todo error
+    send_password_reset_link(user.email)
+    return {"message": 200}
+
+
+def send_password_reset_link(email):
+    password_reset_serializer = URLSafeTimedSerializer(current_app.config['SECRET_KEY'])
+    base_url = current_app.config['FRONT_END_URL'] + '/reset/'
+    base_url += password_reset_serializer.dumps(email, salt='password-reset-salt')
+    # password_reset_url = url_for(
+    #     'auth.token_reset',
+    #     token=password_reset_serializer.dumps(email, salt='password-reset-salt'),
+    #     _external=True)
+    html = render_template('email_reset.html',
+        password_reset_url=base_url)
+
+    send_email('Password Reset Requested', email, html)
+
+
+def send_email(subject, recipients, html_body):
+    message = create_message('TvNet', recipients, subject,  html_body)
+    thr = Thread(target=send_async_email, args=[message])
+    thr.start()
+
+
+def send_async_email(msg):
+    try:
+        send_message(service=service, user_id='me', message=msg)
+    except Exception as e:
+        print(e)
+
+
+def token_reset(token):
+    try:
+        password = request.args.get("password", None)
+        password_reset_serializer = URLSafeTimedSerializer(current_app.config['SECRET_KEY'])
+        email = password_reset_serializer.loads(token, salt='password-reset-salt', max_age=3600)
+    except:
+        return {"msg": "The password reset link is invalid or has expired"}, 401  # todo error
+
+    user = User.query.filter(User.email == email).first()
+    if not user:
+        return {"msg": "Invalid email address!"}, 401  # todo error
+
+    user.hash_password = pbkdf2_sha256.hash(password)
+    db.session.add(user)
+    db.session.commit()
+    return {"message": 200}
 
 
 def logout():
